67daf5e9c2e56a4e5bdbfdb444286ea4b9f968fa
Harold <salatiel_avatar_@hotmail.com>
2022-06-25 12:39:13 -0500

    Add project files.

---
+++ Connection/Database_TXT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Automatic_Report.Connection
+{
+    internal class Database_TXT
+    {
+    }
+}
